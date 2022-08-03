68fa96c584b2927807b32319ea02218c1fc79f43
Dhruv <dhruv.p@simformsolutions.com>
2022-08-03 14:17:22 +0530

    Created entities

---
+++ src/main/java/com/example/postpropertyservice/PostPropertyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.postpropertyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostPropertyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostPropertyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/postpropertyservice/PostPropertyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postpropertyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostPropertyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
