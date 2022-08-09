c7b3362bedcae53e81c821bd81135074ac3ca481
Dhruv <dhruvpatel19201@gmail.com>
2022-08-09 11:55:15 +0530

    Added eureka client configuration

--- src/main/java/com/example/postpropertyservice/PostPropertyServiceApplication.java
+++ src/main/java/com/example/postpropertyservice/PostPropertyServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.postpropertyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PostPropertyServiceApplication {
 
 	public static void main(String[] args) {
